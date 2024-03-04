56673b154026aca27fb40f8063db60ea2a90c184
Phonixoop <ali.hassanzadeh78@gmail.com>
2024-03-04 10:11:02 +0330

    download for last change fixed

--- src/pages/dashboard/personnel_performance/index.tsx
+++ src/pages/dashboard/personnel_performance/index.tsx
@@ -858,42 +858,42 @@ function PersonnelPerformanceTable({ sessionData }) {
                                 };
                               })
                               .filter((f) => f.key != "Id")}
-                            data={personnelPerformance.data.result.map(
-                              (row: any) => {
-                                let Start_Date = "";
-                                if (
-                                  personnelPerformance.data.periodType ===
-                                  "هفتگی"
-                                )
-                                  Start_Date =
-                                    getMonthNamesFromJOINED_date_strings(
-                                      filters.filter.Start_Date.join(","),
-                                      personnelPerformance.data.periodType,
-                                    );
+                            data={(toggleDistinctData === "Distincted"
+                              ? distincedData
+                              : personnelPerformance?.data?.result
+                            ).map((row: any) => {
+                              let Start_Date = "";
+                              if (
+                                personnelPerformance.data.periodType === "هفتگی"
+                              )
+                                Start_Date =
+                                  getMonthNamesFromJOINED_date_strings(
+                                    filters.filter.Start_Date.join(","),
+                                    personnelPerformance.data.periodType,
+                                  );
 
-                                if (
-                                  personnelPerformance.data.periodType ===
-                                  "ماهانه"
-                                )
-                                  Start_Date =
-                                    getMonthNamesFromJOINED_date_strings(
-                                      row.Start_Date,
-                                      personnelPerformance.data.periodType,
-                                    );
+                              if (
+                                personnelPerformance.data.periodType ===
+                                "ماهانه"
+                              )
+                                Start_Date =
+                                  getMonthNamesFromJOINED_date_strings(
+                                    row.Start_Date,
+                                    personnelPerformance.data.periodType,
+                                  );
 
-                                if (
-                                  personnelPerformance.data.periodType ===
-                                  "روزانه"
-                                )
-                                  Start_Date =
-                                    filters.filter.Start_Date.join(",");
+                              if (
+                                personnelPerformance.data.periodType ===
+                                "روزانه"
+                              )
+                                Start_Date =
+                                  filters.filter.Start_Date.join(",");
 
-                                return {
-                                  ...row,
-                                  Start_Date,
-                                };
-                              },
-                            )}
+                              return {
+                                ...row,
+                                Start_Date,
+                              };
+                            })}
                           >
                             دانلود دیتای کامل
                           </CSVLink>
